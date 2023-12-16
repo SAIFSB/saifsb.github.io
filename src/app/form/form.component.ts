5312f64b2af64dc30284d2c14338648b231f1126
Saif <45469661+SAIFSB@users.noreply.github.com>
2023-12-16 21:53:33 +0000

    fixed decimal places

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -17,7 +17,7 @@ export class FormComponent {
 
   onSubmit(form: NgForm){
     this.MonthlyNetIncome = this.calculate(form.value.hourRate, form.value.fuelCostEachLesson, form.value.minLessonDuration, form.value.lessonPerDay, form.value.daysWorkedEachWeek, form.value.insurancePerMonth, form.value.franchiseFee, form.value.otherCostPerMonth)
-    this.WeeklyNetIncome = (this.MonthlyNetIncome*12)/52
+    this.WeeklyNetIncome = Number(((this.MonthlyNetIncome*12)/52).toFixed(2))
   }
 
   calculate(hourRate: number, fuelCostEachLesson: number, minLessonDuration: number, lessonPerDay: number, daysWorkedEachWeek: number,insurancePerMonth: number,  franchiseFee: number, otherCostPerMonth: number): number{
@@ -27,7 +27,7 @@ export class FormComponent {
     let t = (y * 52)/12
     let franchiseAnnual = (franchiseFee * 52)/12
     t = t - (insurancePerMonth + franchiseAnnual + otherCostPerMonth);
-    return t;
+    return Number(t.toFixed(2));
 
   }
 }
