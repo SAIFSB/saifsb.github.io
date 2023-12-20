039d828f8e41637d8fb57c333459c566969f1dce
Saif <45469661+SAIFSB@users.noreply.github.com>
2023-12-20 17:29:44 +0000

    Changes to calculation

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -16,14 +16,21 @@ export class FormComponent {
   }
 
   onSubmit(form: NgForm){
-    this.MonthlyNetIncome = this.calculate(form.value.hourRate, form.value.fuelCostEachLesson, form.value.minLessonDuration, form.value.lessonPerDay, form.value.daysWorkedEachWeek, form.value.insurancePerMonth, form.value.franchiseFee, form.value.otherCostPerMonth)
+    this.MonthlyNetIncome = this.calculateMonthlyNetIncome(form.value.hourRate, form.value.fuelCostEachLesson, form.value.minLessonDuration,
+       form.value.lessonPerDay, form.value.daysWorkedEachWeek, form.value.insurancePerMonth,
+        form.value.franchiseFee, form.value.otherCostPerMonth, form.value.daysWorkedMonday,form.value.daysWorkedTuesday,
+        form.value.daysWorkedWednesday)
+
     this.WeeklyNetIncome = Number(((this.MonthlyNetIncome*12)/52).toFixed(2))
   }
 
-  calculate(hourRate: number, fuelCostEachLesson: number, minLessonDuration: number, lessonPerDay: number, daysWorkedEachWeek: number,insurancePerMonth: number,  franchiseFee: number, otherCostPerMonth: number): number{
+  calculateMonthlyNetIncome(hourRate: number, fuelCostEachLesson: number, minLessonDuration: number,
+     lessonPerDay: number, daysWorkedEachWeek: number,insurancePerMonth: number, 
+      franchiseFee: number, otherCostPerMonth: number,daysWorkedMonday:number,daysWorkedTuesday:number,
+      daysWorkedWednesday:number): number{
   
     let x = (hourRate * minLessonDuration) - fuelCostEachLesson;
-    let y = (x * lessonPerDay) * daysWorkedEachWeek;
+    let y = (daysWorkedMonday+daysWorkedTuesday+daysWorkedWednesday)* x
     let t = (y * 52)/12
     let franchiseAnnual = (franchiseFee * 52)/12
     t = t - (insurancePerMonth + franchiseAnnual + otherCostPerMonth);
