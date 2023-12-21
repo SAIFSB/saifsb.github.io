7aa79948f514f710cdd5428ff54b6b11b46565ff
Saif <45469661+SAIFSB@users.noreply.github.com>
2023-12-21 16:57:37 +0000

    More fields and calculation logic changed with copy function

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,34 +7,76 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
-  
+
   MonthlyNetIncome: number = 0;
   WeeklyNetIncome: number = 0;
 
-  constructor(){
+  constructor() {
 
   }
 
-  onSubmit(form: NgForm){
+  onSubmit(form: NgForm) {
     this.MonthlyNetIncome = this.calculateMonthlyNetIncome(form.value.hourRate, form.value.fuelCostEachLesson, form.value.minLessonDuration,
-       form.value.lessonPerDay, form.value.daysWorkedEachWeek, form.value.insurancePerMonth,
-        form.value.franchiseFee, form.value.otherCostPerMonth, form.value.daysWorkedMonday,form.value.daysWorkedTuesday,
-        form.value.daysWorkedWednesday,form.value.daysWorkedThursday,form.value.daysWorkedFriday,form.value.daysWorkedSaturday,form.value.daysWorkedSunday)
+      form.value.insurancePerMonth, form.value.franchiseFee, form.value.otherCostPerMonth, form.value.carRental, form.value.daysWorkedMonday, form.value.daysWorkedTuesday,
+      form.value.daysWorkedWednesday, form.value.daysWorkedThursday, form.value.daysWorkedFriday, form.value.daysWorkedSaturday, form.value.daysWorkedSunday)
 
-    this.WeeklyNetIncome = Number(((this.MonthlyNetIncome*12)/52).toFixed(2))
+    this.WeeklyNetIncome = Number(((this.MonthlyNetIncome * 12) / 52).toFixed(2))
   }
 
-  calculateMonthlyNetIncome(hourRate: number, fuelCostEachLesson: number, minLessonDuration: number,
-     lessonPerDay: number, daysWorkedEachWeek: number,insurancePerMonth: number, 
-      franchiseFee: number, otherCostPerMonth: number,daysWorkedMonday:number,daysWorkedTuesday:number,
-      daysWorkedWednesday:number,daysWorkedThursday:number,daysWorkedFriday:number,daysWorkedSaturday:number,daysWorkedSunday:number): number{
-  
-    let x = (hourRate * minLessonDuration) - fuelCostEachLesson;
-    let y = (daysWorkedMonday+daysWorkedTuesday+daysWorkedWednesday+daysWorkedThursday+daysWorkedFriday+daysWorkedSaturday+daysWorkedSunday)* x
-    let t = (y * 52)/12
-    let franchiseAnnual = (franchiseFee * 52)/12
-    t = t - (insurancePerMonth + franchiseAnnual + otherCostPerMonth);
-    return Number(t.toFixed(2));
+  calculateMonthlyNetIncome(
+    hourRate: number,
+    fuelCostEachLesson: number,
+    minLessonDuration: number,
+    insurancePerMonth: number,
+    franchiseFee: number,
+    otherCostPerMonth: number,
+    carRental: number,
+    daysWorkedMonday: number,
+    daysWorkedTuesday: number,
+    daysWorkedWednesday: number,
+    daysWorkedThursday: number,
+    daysWorkedFriday: number,
+    daysWorkedSaturday: number,
+    daysWorkedSunday: number):
+    number {
 
+    console.log("carRental value:", carRental);
+
+    let a = (hourRate * minLessonDuration) - fuelCostEachLesson;
+    let b = (daysWorkedMonday + daysWorkedTuesday + daysWorkedWednesday + daysWorkedThursday + daysWorkedFriday + daysWorkedSaturday + daysWorkedSunday) * a
+    let c = (b * 52) / 12
+    let franchiseMonthly = (franchiseFee * 52) / 12
+    let carRentalMonthly = (carRental * 52) / 12
+    console.log("franchiseFeeMonthly value:", franchiseMonthly)
+    console.log("carRentalMonthly value:", carRentalMonthly)
+    let d = c - (insurancePerMonth + franchiseMonthly + carRentalMonthly + otherCostPerMonth);
+    return Number(d.toFixed(2));
+
+  }
+
+  onCopy(form: NgForm) {
+    let allFields = `Hour Rate: ${form.value.hourRate}\n` +
+      `Fuel Cost Each Lesson: ${form.value.fuelCostEachLesson}\n` +
+      `Minimum Lesson Duration: ${form.value.minLessonDuration}\n` +
+      `Insurance Per Month: ${form.value.insurancePerMonth}\n` +
+      `Franchise Fee: ${form.value.franchiseFee}\n` +
+      `Car Rental: ${form.value.carRental}\n` +
+      `Other Cost Per Month: ${form.value.otherCostPerMonth}\n` +
+      `Days Worked Monday: ${form.value.daysWorkedMonday}\n` +
+      `Days Worked Tuesday: ${form.value.daysWorkedTuesday}\n` +
+      `Days Worked Wednesday: ${form.value.daysWorkedWednesday}\n` +
+      `Days Worked Thursday: ${form.value.daysWorkedThursday}\n` +
+      `Days Worked Friday: ${form.value.daysWorkedFriday}\n` +
+      `Days Worked Saturday: ${form.value.daysWorkedSaturday}\n` +
+      `Days Worked Sunday: ${form.value.daysWorkedSunday}\n`  +
+      `Monthly Net Income: ${this.MonthlyNetIncome} \n` +
+      `Weekly Net Income: ${this.WeeklyNetIncome}`;
+
+    // Copy all fields to clipboard
+    navigator.clipboard.writeText(allFields).then(() => {
+      console.log('All fields copied to clipboard');
+    }).catch(err => {
+      console.error('Error in copying text: ', err);
+    });
   }
 }
